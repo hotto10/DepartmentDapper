e990f844d4943f90f928d3b888ffd6b7689f5ac3
heather otto <heatherotto32@yahoo.com>
2022-11-17 15:53:11 -0600

    second commit

---
+++ Dapper/IProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperDem
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> GetAllProducts();
+        public Product GetProduct(int id);
+        public void UpdateProduct(Product product);
+        public void DeleteProduct(int id);
+        void CreateProduct(string name, double price, int categoryID);
+    }
+}
--- Dapper/Program.cs
+++ Dapper/Program.cs
@@ -15,19 +15,50 @@ namespace Dapper
                  .Build();
             string connString = config.GetConnectionString("DefaultConnection");
             IDbConnection conn = new MySqlConnection(connString);
-            var repo = new DapperDepartmentRepository(conn);
 
-            Console.WriteLine("Type a new Department name");
+            #region Departments
+            //var repo = new DapperDepartmentRepository(conn);
 
-            var newDepartment = Console.ReadLine();
+            //Console.WriteLine("Type a new Department name");
 
-            repo.InsertDepartment(newDepartment);
+            //var newDepartment = Console.ReadLine();
 
-            var departments = repo.GetAllDepartments();
+            //repo.InsertDepartment(newDepartment);
 
-            foreach (var dept in departments)
+            //var departments = repo.GetAllDepartments();
+
+            //foreach (var dept in departments)
+            //{
+            //    Console.WriteLine(dept.Name);
+            //}
+            #endregion
+            var productRepository = new DapperProductRepository(conn);
+
+            #region Bonus Update
+            //var productToUpdate = productRepository.GetProduct(940);
+
+            //productToUpdate.Name = "UPDATED";
+            //productToUpdate.Price = 16.99;
+            //productToUpdate.CategoryID = 1;
+            //productToUpdate.OnSale = false;
+
+
+            //productRepository.UpdateProduct(productToUpdate);
+            #endregion
+            productRepository.DeleteProduct(940);
+
+            var products = productRepository.GetAllProducts();
+           
+            foreach (var product in products)
             {
-                Console.WriteLine(dept.Name);
+                Console.WriteLine(product.ProductID);
+                Console.WriteLine(product.Name);
+                Console.WriteLine(product.Price);
+                Console.WriteLine(product.CategoryID);
+                Console.WriteLine(product.OnSale);
+                Console.WriteLine(product.StockLevel);
+                Console.WriteLine();
+                Console.WriteLine();
             }
         }
     }
